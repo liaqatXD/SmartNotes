79ecff861bd845400902b059310c6bc1d95208df
Liaqat Ali <te205255@gmail.com>
2024-05-14 15:04:36 +0500

    imporved user settings

--- app/(app)/settings.jsx
+++ app/(app)/settings.jsx
@@ -1,4 +1,4 @@
-import { View, Button, Text, Switch} from 'react-native'
+import { View, Button, Text, Switch,Image} from 'react-native'
 import {useState,useEffect,useContext} from 'react';
 import AppwriteContext from "../../lib/AppwriteContext";
 import { setUserSession } from '../../asyncStorage';
@@ -6,6 +6,11 @@ import { useColorScheme } from "nativewind";
 import { setTheme } from '../../asyncStorage';
 import { getAccount } from '../../asyncStorage';
 import { StatusBar } from 'expo-status-bar';
+import { Ionicons } from '@expo/vector-icons'; 
+import { Entypo } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const userImage=require("../../assets/images/user.png");
 
 const Settings = () => {
   const { colorScheme, toggleColorScheme } = useColorScheme();
@@ -28,25 +33,73 @@ const Settings = () => {
         setUserSession("false");
     }
   return (
-   (<View className="flex-1 p-6 justify-center bg-white dark:bg-black-dark">
-   <Text>{account.username}</Text>
-  <Text className=" my-4 text-lg font-pregular
-  p-2 dark:text-white">Email:   
-  <Text className="bg-amber-400 dark:text-black">{account.email}
-  </Text></Text>
-  <View className='flex-row'>
-    <Text className='my-4 text-lg font-pregular p-2 dark:text-white'>Dark Mode</Text>
-    <Switch value={colorScheme==='dark'}
+   (<View className="flex-1 p-6  bg-white dark:bg-black-light
+   justify-center">
+
+ <View className="items-center gap-4">
+  <Image
+  source={userImage} style={{width:130,height:130}}
+  />
+ <Text className="text-2xl 
+  font-pextralight
+ dark:text-white">{account.username}</Text>
+ </View>
+
+    {/* username */}
+    <View className="flex-row items-end gap-4 pb-2 mt-4
+     border-gray-300
+     dark:border-gray-100" style={{borderBottomWidth:1}}>
+    <Entypo name="user" size={28} color={colorScheme==='dark'?'#ccc'
+      :'black'
+    }   />
+    <View className="gap-y-2 mt-2">
+      <Text className="text-md font-pregular 
+      tracking-widest
+      dark:text-white">USERNAME</Text>
+      <Text className="text-lg  font-pregular tracking-widest
+      dark:text-white">@{account.username}</Text>
+    </View>
+    </View>
+
+    {/* email */}
+
+    <View className="flex-row items-end gap-4 pb-2 mt-4
+     border-gray-300
+     dark:border-gray-100" style={{borderBottomWidth:1}}>
+   <Ionicons name="mail" size={28} color={colorScheme==='dark'?'#ccc'
+      :'black'} />
+
+    <View className="gap-y-2 mt-2">
+      <Text className="text-md font-pregular 
+      tracking-widest
+      dark:text-white">EMAIL</Text>
+      <Text className="text-lg  font-pregular tracking-widest
+      dark:text-white">{account.email}</Text>
+    </View>
+    </View>
+
+    {/* dark mode */}
+
+    <View className="flex-row items-end gap-4 pb-2 my-4">
+  <MaterialIcons name="dark-mode" size={32} color={colorScheme==='dark'?'#ccc':'black'} style={{marginBottom:8}} />
+
+    <View className="gap-y-2">
+      <Text className="text-md font-pregular 
+      tracking-widest
+      dark:text-white">DARK MODE</Text>
+      <Switch value={colorScheme==='dark'}
     thumbColor={colorScheme==='light'?'black':'white'}
     trackColor={{false: colorScheme==='light'? 'black':'white', true: colorScheme==='dark'? 'white':'black'}}
     onValueChange={()=>{
-      // setDarkMode(colorScheme==='light'?true:false);
       toggleColorScheme();
       if(colorScheme==='light') setTheme('dark');
       else setTheme('light');
     }} 
     />
-  </View>
+    </View>
+    </View>
+
+{/* logout button */}
  <Button title='Logout' onPress={logout} />
  <StatusBar style={colorScheme==='light'?'dark':'light'} />
 </View>)
